ac7f400b9394378af85a7cd76a9bfeb08951001a
minhnhut571 <minhnhut05072001@gmail.com>
2023-06-30 03:14:57 +0700

    Revert "Merge branch 'main' of https://github.com/minhnhut571/CapstoneProject-BIDs-BE-WEB"
    This reverts commit b126515c2b18b981137fb869c4b155f604edf22b, reversing
    changes made to 9ec139cbca564a2de585297aee06e48f9f440963.

--- CapstoneProject-BIDs/BIDs-API/Controllers/UsersController.cs
+++ CapstoneProject-BIDs/BIDs-API/Controllers/UsersController.cs
@@ -12,7 +12,6 @@ namespace BIDs_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
